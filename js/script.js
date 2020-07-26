81fcff65d5154811dabd62c1c03acc71c2f9bd62
iROMAN <niewiadomski72@gmail.com>
2020-07-26 11:45:40 +0200

    update radio

--- js/script.js
+++ js/script.js
@@ -68,7 +68,7 @@ new Vue({
           name: ":)",
           artist: "Radio",
           cover: "./musicgraphic/radio.jpg",
-          source: "http://pldm.ml/radio.php?url=https://www.eskago.pl/radio/eska-warszawa",
+          source: "http://waw01-01.ic.smcdn.pl:8000/t042-1.mp3",
           url: "https://www.youtube.com/watch?v=oTUaBIOyLrM",
           favorited: false
         },
