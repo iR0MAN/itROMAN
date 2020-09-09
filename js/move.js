ab63f07fae0ba9e234fb8a2a09ceb56208d915b5
iROMAN <niewiadomski72@gmail.com>
2020-09-09 15:08:46 +0200

    update hamburger animation

--- js/move.js
+++ js/move.js
@@ -1,6 +1,7 @@
 $('li button').on('click', function () {
     const goToSection = "[data-section=" + $(this).attr('class') + "]";
     $(".navigation").toggleClass('nav-off');
+    burger.classList.toggle("is-active");
     $('body, html').animate({
         scrollTop: $(goToSection).offset().top
     })
