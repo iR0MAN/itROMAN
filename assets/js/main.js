d98a3e2afc5b88e1de4c5057e25e9fced5c0f895
iR0MAN <62302540+iR0MAN@users.noreply.github.com>
2023-04-16 14:38:00 +0200

    main js update
    email server update

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,5 +267,34 @@
    */
   new PureCounter();
 
+/**
+   * EMAIL
+   */
+
+  function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      subject: document.getElementById("subject").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_cy9dr3d";
+    const templateID = "service_cy9dr3d";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("subject").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+          alert("Your message sent successfully!!")
+  
+      })
+      .catch(err=>console.log(err));
+  
+  }
+
 
 })()
