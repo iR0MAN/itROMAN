608222cfdeddab31a52f867e911a250c48445c72
iR0MAN <62302540+iR0MAN@users.noreply.github.com>
2023-04-18 20:32:16 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -267,4 +267,13 @@
    */
   new PureCounter();
 
+
+  app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+
 })()
